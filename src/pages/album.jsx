d88f282f7628b0428798a2e527d49f66c15d45a0
Cortada <caio.cortada@unesp.br>
2022-09-05 22:13:57 -0300

    Feat: paginas individuais

--- src/pages/album.jsx
+++ src/pages/album.jsx
@@ -1,25 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Image, TextInput ,Alert, KeyboardAvoidingView,ScrollView, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Image, TextInput ,Alert, KeyboardAvoidingView,ScrollView,RefreshControl, SafeAreaView} from 'react-native';
 import React, { useEffect, useState } from 'react';
 import styles from '../style/styles';
 import axios from '../../axios';
 
 
-
 export default function Album(props){
 
-    const metadeTela = Dimensions.get('window').width / 2 * 0.95;
+    const [refresh,setRefresh] = useState(false);
+
+    const pullMe = () => {
+
+        setRefresh(true)
+
+        setTimeout(() =>{
+            setRefresh(false)
+        },10000)
+
+
+
+    }
+
 
     const [vetor, setArray] = useState([]);
-    const [codfig, setCodfig] = useState('')
+    const [codfig, setCodfig] = useState('');
 
-    useEffect(() => {
+    useEffect(()=>{
         listFig();
-      }, []);
+      },[])
+      
 
-      const listFig = async () => {
+      function listFig() {
 
-        const result2 = await axios.get('/fig/listatodos')
+        const result2 =  axios.get('/fig/listatodos')
         .then((result2) =>{
             console.log(result2.data)
             setArray(result2.data)
@@ -30,11 +43,22 @@ export default function Album(props){
 
 
     }
-    
-    function cadastrarFig(param){
 
-            setCodfig(param);
-            console.log(param)
+    function postFiguras(paramfig){
+
+        console.log(paramfig);
+
+        const result = axios.post('/fig/cadastrar',paramfig)
+        .then((result) => {
+            
+
+            Alert.alert("Sucesso",result.data,
+           
+              )
+          }).catch((error) => {
+            Alert.alert("ERRO","FIGURINHA NÃO EXISTENTE")
+          });
+  
         }
 
     return (
@@ -42,7 +66,13 @@ export default function Album(props){
         behavior={Platform.OS === "ios" ? "padding" : "height"}
             style={styles.container}
             keyboardVerticalOffset={70}>
-            <ScrollView style={{width:'100%'}}>
+            <ScrollView style={{width:'100%'}}
+            refreshControl={
+                <RefreshControl
+                  refreshing={refresh}
+                  onRefresh={()=> pullMe()}
+                />
+              }>
             <SafeAreaView style={styles.containerAlbum}>
                 <StatusBar style="auto" />
 
@@ -52,7 +82,7 @@ export default function Album(props){
                             return (
                                
 
-                                    <TouchableOpacity key={array.CODIGO} style={{backgroundColor:'rgba(20,80,20,.3)', borderColor: 'rgba(20,70,20,.3)', marginTop:20,marginHorizontal:5 , borderWidth: 2, padding: 0, flexDirection: 'column', width: '25%',height:40, borderRadius: 10 }}>
+                                    <TouchableOpacity key={array.CODIGO} style={{backgroundColor:'rgba(20,80,20,.3)', marginTop:20,marginHorizontal:5 , borderWidth: 2, padding: 0, flexDirection: 'column', width: '25%',height:40, borderRadius: 10 }}>
                                     <View style={{ width: "100%", alignContent: 'center', alignItems: 'center' }}>
                                         <Text style={{ fontWeight: 'bold', textAlign: 'center',fontSize:10 }}>
                                             {array.PAGINA}
@@ -68,7 +98,7 @@ export default function Album(props){
                                    
                             );}else{
                                 return (
-                                <TouchableOpacity key={array.CODIGO} onPress={cadastrarFig(array.codigo)} style={{ backgroundColor:'rgba(80,20,20,.3)', marginTop:20,marginHorizontal:5 , borderWidth: 2, padding: 0, flexDirection: 'column', width: '25%',height:40, borderRadius: 10 }}>
+                                <TouchableOpacity key={array.CODIGO} onPress={() => postFiguras(array)} style={{ backgroundColor:'rgba(80,20,20,.3)', marginTop:20,marginHorizontal:5 , borderWidth: 2, padding: 0, flexDirection: 'column', width: '25%',height:40, borderRadius: 10 }}>
                                     <View style={{ width: "100%", alignContent: 'center', alignItems: 'center' }}>
                                         <Text style={{ fontWeight: 'bold', textAlign: 'center',fontSize:10 }}>
                                             {array.PAGINA}
---
+++ src/pages/paginas/belgica.jsx
@@ -0,0 +1,39 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Image, TextInput ,Alert, KeyboardAvoidingView,ScrollView, SafeAreaView} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import styles from '../../style/styles';
+import { TextInputMask } from 'react-native-masked-text';
+import axios from '../../../axios';
+
+import {cromos} from '../../component/cromos'
+
+
+export default function Qatar(props) {
+
+   
+    return (
+
+
+        <KeyboardAvoidingView 
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.container}
+            keyboardVerticalOffset={70}>
+            <ScrollView style={{width:'100%'}}>
+            <SafeAreaView style={styles.container}>
+
+           <Text>qatar</Text>
+
+
+
+
+                </SafeAreaView>
+                </ScrollView>
+        </KeyboardAvoidingView>
+        
+
+
+
+
+    );
+
+}
