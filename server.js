ac173701b228fd7738d563d75c5fd058a1a631ed
Cortada <caio.cortada@unesp.br>
2022-09-07 18:44:59 -0300

    Config: express

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+
+const app = express()
+
+const { resolve } = require('path')
+
+
+app.use('/', express.static(
+    
+    resolve(__dirname, './web-build')
+    
+    
+    ))
+
+
+app.listen(process.env.PORT || 3000, (err)=>{
+    if(err){return console.log(err)}
+
+    console.log('TUDO CERTO')
+
+}
+
+)
